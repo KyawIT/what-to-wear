f76f8f5697db3f47fe702b684462a72846392301
KyawIT <kyawhtethtun65@gmail.com>
2025-12-15 01:52:47 +0100

    feat: added desc. and logo to main screen

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import {Text, View} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {Image} from "expo-image";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <SafeAreaView>
+            <View className={"h-screen-safe justify-center items-center bg-primary-0"}>
+                <View className="rounded-2xl items-center justify-center">
+                    <Image
+                        source={require("../assets/logo/logo-lg.png")}
+                        style={{ width: 120, height: 120 }}
+                        contentFit="contain"
+                    />
+                </View>
+                <Text className={"text-2xl"}>
+                    <Text className={"text-indigo-500 font-extrabold"}>What</Text>
+                    <Text className={"text-red-500 font-extrabold"}>To</Text>
+                    <Text className={"text-indigo-500 font-extrabold"}>Wear</Text>
+                </Text>
+                <Text className={"text-xl font-bold"}>Scan it. Style it. Wear it.</Text>
+                <Text className={"text-lg text- p-5"}>
+                    Digitize your wardrobe, create outfits, and get fresh AI inspiration anytime.
+                    Your clothes — finally organized.
+                </Text>
+            </View>
+        </SafeAreaView>
+    );
 }
