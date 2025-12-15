80309c409bfc6eadc8efb7e3eaf910dcb1ce5d40
KyawIT <kyawhtethtun65@gmail.com>
2025-12-15 01:52:15 +0100

    feat: wrapped SafeAreaProvider and GlueStack in layout

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Stack } from "expo-router";
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+import {authClient} from "@/lib/auth-client";
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import {StatusBar} from "expo-status-bar";
+
 export default function RootLayout() {
-  return <Stack />;
+  const {data: isAuthenticated} = authClient.useSession();
+  return (
+      <SafeAreaProvider>
+          <GluestackUIProvider>
+              <StatusBar hidden={true}/>
+              <Stack screenOptions={{headerShown: false}}/>
+          </GluestackUIProvider>
+      </SafeAreaProvider>
+  );
 }
