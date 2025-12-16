71903cacca17ac2c8f11369f2fd5fd6f4f062ac4
KyawIT <kyawhtethtun65@gmail.com>
2025-12-16 23:11:19 +0100

    refactor: code refactored

--- frontend/app/api/auth/[...route]+api.ts
+++ frontend/app/api/auth/[...route]+api.ts
@@ -1,9 +1,3 @@
-import { auth } from "@/lib/auth";
-
-export const GET = (request: Request) => {
-    return auth.handler(request);
-};
-
-export const POST = (request: Request) => {
-    return auth.handler(request);
-};
+import {auth} from "@/lib/auth"
+const handler = auth.handler;
+export { handler as GET, handler as POST };
