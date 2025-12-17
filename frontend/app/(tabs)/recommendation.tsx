f2ac9c0edc2698a64e7079a1fcb5924fd0da4b67
KyawIT <kyawhtethtun65@gmail.com>
2025-12-17 01:27:45 +0100

    feat: added new component recommandation.tsx

---
+++ frontend/app/(tabs)/recommendation.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const Recommendation = () => {
+    return (
+        <View>
+            <Text>Recommendation</Text>
+        </View>
+    )
+}
+export default Recommendation
