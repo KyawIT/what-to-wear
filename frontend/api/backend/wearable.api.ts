a60ba4359258195761dfa4d757d1ba352ac2aa52
KyawIT <kyawhtethtun65@gmail.com>
2026-01-14 00:30:15 +0100

    feat(frontend): add new fetch function to get wearable by id

--- frontend/api/backend/wearable.api.ts
+++ frontend/api/backend/wearable.api.ts
@@ -3,7 +3,56 @@ import {WearableCategory, WearableResponseDto} from "@/api/backend/wearable.mode
 const BASE_URL = (process.env.EXPO_PUBLIC_BACKEND_ROOT ?? "http://localhost:8080")
     .replace(/\/+$/, "");
 
-const ENDPOINT = "/api/wearable/by-category";
+const ENDPOINT = "/api/wearable";
+const ENDPOINT_BY_CATEGORY = "/api/wearable/by-category";
+
+export async function fetchAllWearables(
+    userId: string
+): Promise<WearableResponseDto[]> {
+    if (!userId || !userId.trim()) {
+        throw new Error("X-User-Id is required");
+    }
+
+    const res = await fetch(`${BASE_URL}${ENDPOINT}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "X-User-Id": userId.trim(),
+        },
+    });
+
+    if (!res.ok) {
+        const text = await res.text();
+        throw new Error(
+            `Failed to fetch wearables (${res.status}): ${text}`
+        );
+    }
+
+    return res.json();
+}
+
+export async function fetWearableById(userId: string, wearableId: string): Promise<WearableResponseDto> {
+    if (!userId || !userId.trim()) {
+        throw new Error("X-User-Id is required");
+    }
+
+    const res = await fetch(`${BASE_URL}${ENDPOINT}/${wearableId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "X-User-Id": userId.trim(),
+        },
+    });
+
+    if (!res.ok) {
+        const text = await res.text();
+        throw new Error(
+            `Failed to fetch wearables (${res.status}): ${text}`
+        );
+    }
+
+    return res.json();
+}
 
 export async function fetchWearablesByCategory(
     userId: string,
@@ -18,7 +67,7 @@ export async function fetchWearablesByCategory(
     }
 
     const url =
-        `${BASE_URL}${ENDPOINT}?category=${encodeURIComponent(category)}`;
+        `${BASE_URL}${ENDPOINT_BY_CATEGORY}?category=${encodeURIComponent(category)}`;
 
     const res = await fetch(url, {
         method: "GET",
