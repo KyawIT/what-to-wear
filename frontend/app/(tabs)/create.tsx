157541d6b348df9becf312455f2e81368a85a8f9
KyawIT <kyawhtethtun65@gmail.com>
2026-02-19 21:00:35 +0100

    feat(frontend): setup create screen

--- frontend/app/(tabs)/create.tsx
+++ frontend/app/(tabs)/create.tsx
@@ -1,7 +1,25 @@
 import {View, Text} from 'react-native'
-import React from 'react'
+import React, {useEffect} from 'react'
+import {authClient} from "@/lib/auth-client";
+import {getKeycloakAccessToken} from "@/lib/keycloak";
+import {fetchAllWearables} from "@/api/backend/wearable.api";
 
 const Create = () => {
+    const { data } = authClient.useSession();
+
+    useEffect(() => {
+        if (!data?.user?.id) return;
+
+        (async () => {
+            try {
+                const accessToken = await getKeycloakAccessToken(data.user.id);
+                console.log(accessToken)
+            } catch (err) {
+                console.error("Failed to fetch total wearables:", err);
+            }
+        })();
+    }, [data?.user?.id]);
+
     return (
         <View>
             <Text>Create</Text>
