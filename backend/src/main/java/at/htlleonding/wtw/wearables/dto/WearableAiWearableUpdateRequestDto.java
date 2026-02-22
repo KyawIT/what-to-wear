8cab0fe6cebe33cd72d594635e70b29614cb27fe
KyawIT <kyawhtethtun65@gmail.com>
2026-02-22 02:52:30 +0100

    feat(backend) add WearableAiWearableUpdateRequestDto.java to support outfit recommendation and service integration

---
+++ backend/src/main/java/at/htlleonding/wtw/wearables/dto/WearableAiWearableUpdateRequestDto.java
@@ -0,0 +1,8 @@
+package at.htlleonding.wtw.wearables.dto;
+
+public class WearableAiWearableUpdateRequestDto {
+    public String user_id;
+    public String item_id;
+    public String category;
+    public String tags;
+}
